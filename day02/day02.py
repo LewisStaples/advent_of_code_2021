5549a35631eea9705bb757b858f95e5169c25be3
Lewis Staples <lewis.staples@gmail.com>
2021-12-02 00:40:25 -0500

    Day 2: part a working

--- day02/day02.py
+++ day02/day02.py
@@ -2,7 +2,7 @@
 # https://adventofcode.com/2021/day/2
 
 # Task 1:  Input Data
-input_filename='input_sample0.txt'
+input_filename='input.txt'
 input_list = []
 
 with open(input_filename) as f:
@@ -14,8 +14,24 @@ with open(input_filename) as f:
             print('Error! Bad number: ' + number)
         input_list.append((direction,int(number)))
 
-print(input_list)
+# print(input_list)
 
 
+# Task 2:  Solve part a
+position = [0,0] # horizontal position, depth
+for (direction, number) in input_list:
+    # print(direction + ', ' + str(number))
+    if direction == 'forward':
+        position[0] += number
+    if direction == 'down':
+        position[1] += number
+    if direction == 'up':
+        position[1] -= number
+
+# print('Final position is: ', end='')
+# print(position)
+
+print('The answer to part a is: ', end='')
+print(position[0] * position[1])
 
 
