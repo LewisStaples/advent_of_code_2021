e8712f16d8a5ac1248e8ae8716da892c839deea3
Lewis Staples <lewis.staples@gmail.com>
2021-12-01 19:32:36 -0500

    Day 1 complete

---
+++ day01/day01.py
@@ -0,0 +1,59 @@
+# adventOfCode 2021 day 1, parts 1 and 2
+# https://adventofcode.com/2021/day/1
+
+
+# Task 1:  Input Data
+input_filename='input.txt'
+input_numbers = []
+
+with open(input_filename) as f:
+    for in_string in f:
+        in_num = int(in_string)
+        input_numbers.append(in_num)
+
+# print out the input
+# print()
+# print(input_numbers)
+
+
+# Task 2:  Solve part a
+total_increases = 0
+
+# print()
+# print()
+
+for i in range(len(input_numbers)-1):
+    # print(input_numbers[i])
+    if input_numbers[i+1] > input_numbers[i]:
+        total_increases += 1
+
+print()
+print('The answer to part a is:', end=' ')
+print(total_increases)
+print()
+
+
+# Task 3:  Solve part b
+total_increases = 0
+
+# print()
+# print()
+
+for i in range(len(input_numbers)-3):
+    # print(input_numbers[i])
+    # print(input_numbers[i+3])
+    # print()
+
+    # to evaluate if the latter "three-measurement sliding window" 
+    # is larger than the prior, it is OK to only compare the prior's 
+    # earliest member and the latter's latest member, because the other
+    # members are in both windows, so their effects are cancelled out
+    if input_numbers[i+3] > input_numbers[i]:
+        total_increases += 1
+
+print()
+print('The answer to part b is:', end=' ')
+print(total_increases)
+print()
+
+
