36b8e14a4d9e9cd1dd23e3b7efec2a9356ca8287
Lewis Staples <lewis.staples@gmail.com>
2021-12-08 00:32:18 -0500

    Day 8: part a working

---
+++ day08/day08.py
@@ -0,0 +1,21 @@
+# adventOfCode 2021 day 8
+# https://adventofcode.com/2021/day/8
+
+input_filename='input.txt'
+
+# appearances of 1, 4, 7, or 8  (char lengths: 2, 4, 3, 7)
+unique_seg_digit_count = 0
+
+# print()
+with open(input_filename) as f:
+    # pull in each line from the input file
+    for in_string in f:
+        [signal_pattern, output_value] = in_string.split(' | ')
+        output_value = output_value.rstrip()
+        for output_digit in output_value.split(' '):
+            if len(output_digit) in [2,4,3,7]:
+                unique_seg_digit_count += 1
+
+print()
+print('The answer to part a is ', end='')
+print(unique_seg_digit_count)
