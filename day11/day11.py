8f65035ce0047e1680727b51f4ed7a06d5a2aa8d
Lewis Staples <lewis.staples@gmail.com>
2021-12-11 20:50:58 -0500

    Day 11 code cleanup

--- day11/day11.py
+++ day11/day11.py
@@ -1,12 +1,22 @@
 # adventOfCode 2021 day 11
 # https://adventofcode.com/2021/day/11
 
+
 import enum
+
+# this enum defines the states of an octopus during a step:
 class FlashStatus(enum.Enum):
+    # it hasn't flashed yet in the current step
     NOT_FLASHED = 0
+
+    # it has just flashed
     JUST_FLASHED = 1
+
+    # it flashed earlier (the adjacent octpuses already had energy increased)
     PREVIOUSLY_FLASHED = 2
 
+
+# this class defines the status of a particular octopus
 class OctopusStatus:
     def __init__(self, energy):
         self.energy = energy
@@ -15,29 +25,37 @@ class OctopusStatus:
         # total number of times that this octopus has flashed
         self.flash_tally = 0
 
+    # get the current value of energy
     def get_energy(self):
         return self.energy
     
+    # this determines if the octopus has been flashed
     def get_flashed(self):
         return self.flashed
 
-    # total number of times that this octopus has flashed
+    # get total number of times that this octopus has flashed
     def get_flash_tally(self):
         return self.flash_tally
 
+    # increase energy, update associated flash statuses
     def increment_energy(self):
         self.energy += 1
         if self.energy == 10:
             self.flashed = FlashStatus.JUST_FLASHED
             self.flash_tally += 1
 
+    # update flash status to PREVIOUSLY_FLASHED
     def post_flashed(self):
         self.flashed = FlashStatus.PREVIOUSLY_FLASHED
 
+    # (at the end of a step) reset octopus status
     def reset_energy(self):
         self.energy = 0
         self.flashed = FlashStatus.NOT_FLASHED
 
+
+# this class holds the data structure for all octopuses,
+# and implements associated logic
 class Octopus:
     def __init__(self):
         self.oct_status = []
@@ -64,6 +82,7 @@ class Octopus:
                 ret_val += self.oct_status[i][j].get_flash_tally()
         return ret_val
 
+    # this displays a grid display of octopus energies
     def display(self):
         for line in self.oct_status:
             str_list = line
@@ -71,9 +90,9 @@ class Octopus:
                 print(oct.get_energy(), end='')
             print()
 
-    # line is a string of digits
+    # this is used when importing energy numbers into a row of octopuses
+    # line is a string of single digit integers, representing energy
     def add_line(self, line):
-        # self.oct_status.append(oct_list)
         oct_line = []
         for digit in line:
             oct_line.append(OctopusStatus(int(digit)))
@@ -99,6 +118,7 @@ class Octopus:
 
     # this function resets the energy to zero of any 
     # octopuses whose energy was greater than nine
+    # (this is designed to be used at the end of a step)
     def reset_to_zero(self):
         for i in range(len(self.oct_status)):
             for j in range(len(self.oct_status[i])):
@@ -117,6 +137,8 @@ class Octopus:
     def flashes(self):
         while True:
             flash_count = 0
+            # look for any octopuses that just flashed
+            # if found, increment the adjacent octopuses
             for i in range(len(self.oct_status)):
                 for j in range(len(self.oct_status[i])):
                     if self.oct_status[i][j].get_flashed() == FlashStatus.JUST_FLASHED:
@@ -129,11 +151,12 @@ class Octopus:
                         flash_count += self.increment_one_oct(i+1,j-1)
                         flash_count += self.increment_one_oct(i+1,j)
                         flash_count += self.increment_one_oct(i+1,j+1)
+            # stop repeating if no new flashes have happened
             if flash_count == 0:
                 break
 
-    # this function performs a single step
-    # the return value (for part b) is whether all octpuses are lit at this time
+    # this function performs a single step (for parts a and b)
+    # the return value (for part b only) is whether all octpuses are lit at this time
     # all other information passed to the calling program are embedded in the 
     # octopus' oct_status data structure
     def perform_step(self):
@@ -144,7 +167,8 @@ class Octopus:
         self.reset_to_zero()
         return ret_val
 
-# reading height input from the input file
+
+# reading energy input from the input file
 input_filename='input.txt'
 octopus = Octopus()
 with open(input_filename) as f:
@@ -152,21 +176,30 @@ with open(input_filename) as f:
     for in_string in f:
         octopus.add_line(in_string.rstrip())
 
+# if desired, the initial state could be displayed
 # print()
 # print('Initial state:')
 # octopus.display()
+
 print()
 
-for i in range(1000):
+# range parameter needs to be set high enough to deliver answers to a and b
+for i in range(327):
+
+    # this calls the code to perform the step
+    # and it checks if all octopuses are lit up
     if octopus.perform_step():
         print('Step# ' + str(i+1) + ' has all octopuses illuminated')
-    # if i in [192,193, 194]:  # need to type in one less than desired step number
-    #     print('After step # ' + str(i+1))
-    #     octopus.display()
-    #     print()
 
-print('The answer to part a is ', end='')
-print(octopus.get_flash_tally())
+    # add numbers to list for steps you wish to display
+    # the step number and the whole grid of energies will be shown
+    # you need to type in one less than the step number(s) to display in the list
+    if i in [99, 323]:  
+        print('After step # ' + str(i+1) + ', the number of flashes is ', end='')
+        print(octopus.get_flash_tally())        
+        print()
 
-
+        octopus.display()
+        print()
+        print()
 
