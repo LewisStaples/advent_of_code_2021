6dba0ffa2e6a8edc6337e626f9cab706f3a22242
Lewis Staples <lewis.staples@gmail.com>
2021-12-20 01:45:23 -0500

    Day 18 switched to use string as the model (internal representation):  explode seems to be working

--- day18/day18.py
+++ day18/day18.py
@@ -1,120 +1,107 @@
 # adventOfCode 2021 day 18
 # https://adventofcode.com/2021/day/18
 
-# class SFN_Pair:
-
 import sys
-
-class Node:
-    def __init__(self, parentNode):
-        self.values = []
-        if parentNode is not None:
-            self.parent = parentNode
-            self.parent.child = self
-
-    def get_magnitude(self):
-        mult_factor = [3,2]
-        ret_val = 0
-        for index in range(0,2):
-            element = self.values[index]
-            ret_val += mult_factor[index] * (element if isinstance(element, int) else element.get_magnitude())
-        return ret_val
-
-    def explode(self, levels=1):
-        ret_val = 0
-        if levels == 4:
-            for i, value in enumerate(self.values):
-                if value is Node:
-                    i_change = (i + 1) % 2
-                    self.values[i_change] += value.values[i_change]
-                    value = 0
-                    return 1
-
-        for value in self.values:
-            if value is Node:
-                ret_val += value.explode(levels+1)
-        return ret_val
-
-    def split(self):
-        ret_val = 0
-        for value in self.values:
-            if isinstance(value, int):
-                if value > 9:
-                    # do something neat-o !!!!!!!!!!!!!!!!!!!!
-                    return 1
-            elif value is Node:
-                ret_val += self.split()
-
-        return ret_val
-# end of class Node
+# import re
 
 class SnailfishNumber:
-    def __init__(self, snnum_string):
-        print(snnum_string)
-        print()
-        self.root = Node(None)
-        pointer = self.root
-        for char in snnum_string[1:-1]:
-            if char == '[':
-                pointer.values.append(Node(pointer))
-                pointer = pointer.values[-1]
-            elif char == ']':
-                pointer = pointer.parent
-            elif char.isdigit():
-                if len(pointer.values) > 1:
-                    sys.exit('Error!  Putting more than two values in a pair')
-                pointer.values.append(int(char))
-            
-    def add(self, other_node):
-        left_node = self.root
-        self.root = Node(None)
-        self.root.values.append(left_node)
-        self.root.values.append(other_node)
+    def __init__(self, in_str):
+        self.sfnum = in_str
+
+    def display(self):
+        print(self.sfnum)
+
+    def add(self, other_sfnum):
+        # safety check
+        # if other_sfnum is not SnailfishNumber:
+        if not isinstance(other_sfnum, SnailfishNumber):
+            sys.exit('cannot add other objects to a S.F.Number')
+        self.sfnum = '[' + self.sfnum + ',' + other_sfnum.sfnum + ']'
+
+# (element if isinstance(element, int) else element.get_magnitude())
 
     def get_magnitude(self):
-        return self.root.get_magnitude()
+        return 0
 
-    def explode(self):
-        return self.root.explode()
+    # This function explodes a pair that starts at index i
+    # This assumes that the string starting at i will be
+    # opening bracket number 1, comma, number 2, closing bracket
+    def do_explosion(self, i, j):
+        # get numbers in the pair that will be exploded
+        [n1, n2] = self.sfnum[i+1:j].split(',')
+        [n1, n2] = [int(n1), int(n2)]
+        left_string = self.sfnum[:i]
+        right_string = self.sfnum[j+1:]
+
+        # seek last number in left_string (if found, add n1)
+        for index_last in range(i-1,-1,-1):
+            if left_string[index_last].isdigit():
+                for index_first in range(index_last,-1,-1):
+                    if not left_string[index_first].isdigit():
+                        old_value = int(left_string[index_first+1:index_last+1])
+                        left_string = left_string[0:index_first+1] + \
+                        str(old_value + n1) + left_string[index_last+1:]
+                        break # break out of inner for loop
+                break # break out of outer for loop
+
+        # seek first number in right string (if found add n2)
+        for index_first in range(0,len(right_string)):
+            if right_string[index_first].isdigit():
+                for index_last in range(index_first,len(right_string)):
+                    if not right_string[index_last].isdigit():
+                        old_value = int(right_string[index_first:index_last])
+                        right_string = right_string[0:index_first] + \
+                        str(old_value + n2) + right_string[index_last:]
+                        break
+                break
+        
+        # assemble the pieces together
+        self.sfnum = left_string + '0' + right_string
+        # end of method do_explosion
+
+    # This function looks if an explosion is needed
+    # If one is needed, it calls do_explosion
+    # It assumes that the pair to be exploded is 
+    # opening bracket number 1, comma, number 2, closing bracket
+    def seek_explosion(self):
+        level = 0
+        for i, ch in enumerate(self.sfnum):                
+            if ch == '[':
+                level += 1
+                if level == 5:
+                    j = self.sfnum.find(']', i)
+                    self.do_explosion(i,j)
+                    return True
+            elif ch == ']':
+                level -= 1
+        return False
 
     def split(self):
-        return self.root.split()
+        return
 
     def reduce(self):
         while True:
-            sum_ops = 0
-            sum_ops += self.explode()
-            sum_ops += self.split()
-            if sum_ops == 0:
-                break
+            if self.seek_explosion():
+                continue # explosion occured, so repeat 
+            self.split()
+            return # finished reduction
 
 # end of definition of Class SnailfishNumber
 
-list_snailfish_number_strings = []
-
-# reading input from the input file
-input_filename='input.txt'
-with open(input_filename) as f:
-    # pull in each line from the input file
-    for in_string in f:
-        # print(in_string.rstrip())
-        list_snailfish_number_strings.append(in_string.rstrip())
+# n1 = SnailfishNumber('[1,2]')
+# n2 = SnailfishNumber('[[3,4],5]')
+# n1.add(n2)
+# n1.display()
 
 
-# for sn_str in list_snailfish_number_strings:
-#     print(sn_str)
+sn = SnailfishNumber('[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]')
+sn.display()
+sn.seek_explosion()
+sn.display()
 
-# sfnum = SnailfishNumber(list_snailfish_number_strings[0])
-
-# sfnum = SnailfishNumber('[[[[5,0],[7,4]],[5,5]],[6,6]]')
-sfnum = SnailfishNumber('[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]')
-# sfnum = SnailfishNumber('')
-# sfnum = SnailfishNumber('')
-# sfnum = SnailfishNumber('')
-
-# sfnum = SnailfishNumber('[[[[3,0],[5,3]],[4,4]],[5,5]]')
-
-sfnum.reduce()
-print(sfnum.get_magnitude())
+# sn = SnailfishNumber('[[6,[5,[4,[3,2]]]],1]')
+# sn.display()
+# sn.seek_explosion()
+# sn.display()
 
 
