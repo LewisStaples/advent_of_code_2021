294f6535b5917741f455199b823751c0c020a929
Lewis Staples <lewis.staples@gmail.com>
2022-01-12 23:24:14 -0500

    Day 22 Part A Complete

--- day22/day22.py
+++ day22/day22.py
@@ -2,6 +2,7 @@
 # https://adventofcode.com/2021/day/22
 
 input_cube_reset_instructions = []
+reactor = set()
 
 class CubeResetInstruction:
     # Load a line of input instructions into memory
@@ -21,10 +22,23 @@ class CubeResetInstruction:
             if self.ranges[axis][0] > self.ranges[axis][1]:
                 self.operation = False
                 break
+    # End of __init__ declaration
 
-# reading input from the input file
-input_filename='input_scenario0.txt'
+    # Execute this one instruction
+    def execute(self):
+        for i in range(self.ranges['x'][0], 1 + self.ranges['x'][1]):
+            for j in range(self.ranges['y'][0], 1 + self.ranges['y'][1]):
+                for k in range(self.ranges['z'][0], 1 + self.ranges['z'][1]):
+                    this_cube = (i,j,k)
+                    if self.operation == 'on':
+                        reactor.add(this_cube)
+                    if self.operation == 'off':
+                        reactor.discard(this_cube)
+
+# Reading input from the input file
+input_filename='input.txt'
 with open(input_filename) as f:
+    del input_filename
     # pull in each line from the input file
     for in_string in f:
         in_string = in_string.rstrip()
@@ -32,5 +46,14 @@ with open(input_filename) as f:
         if cri.operation:
             input_cube_reset_instructions.append(cri)
 
-pass
+# Delete variables that are no longer needed
+del cri
+del in_string
+del f
+
+for icri in input_cube_reset_instructions:
+    icri.execute()
+
+print('The answer to part A is: ', end='')
+print(len(reactor))
 
