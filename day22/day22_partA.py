7807ac3cfe15bb187ea84d6e43831fb172b98c03
Lewis Staples <lewis.staples@gmail.com>
2022-01-16 00:11:30 -0500

    Day 22 Improvements to comments

--- day22/day22_partA.py
+++ day22/day22_partA.py
@@ -1,9 +1,17 @@
 # adventOfCode 2021 day 22
 # https://adventofcode.com/2021/day/22
 
+# This list stores reset instructions in memory.
+# (This is, in case, future instructions need to manipulate them, such as 
+# needing to run these instructions backward)
 input_cube_reset_instructions = []
+
+# This set is a set of 3_tuples with the coordinates of all cubes that are on
 reactor = set()
 
+# This class is used to load the input instructions into memory.
+# (This was developed in part A, under the incorrect assumption 
+# that this might perhaps be useful in part B)
 class CubeResetInstruction:
     # Load a line of input instructions into memory
     def __init__(self, str_input):
@@ -26,9 +34,11 @@ class CubeResetInstruction:
 
     # Execute this one instruction
     def execute(self):
+        # Loop through all three axes
         for i in range(self.ranges['x'][0], 1 + self.ranges['x'][1]):
             for j in range(self.ranges['y'][0], 1 + self.ranges['y'][1]):
                 for k in range(self.ranges['z'][0], 1 + self.ranges['z'][1]):
+                    # Update set reactor to indicate the status of this cube.
                     this_cube = (i,j,k)
                     if self.operation == 'on':
                         reactor.add(this_cube)
--- day22/day22_partB.py
+++ day22/day22_partB.py
@@ -1,9 +1,17 @@
 # adventOfCode 2021 day 22
 # https://adventofcode.com/2021/day/22
 
+# This list stores reset instructions in memory.
+# (This is, in case, future instructions need to manipulate them, such as 
+# needing to run these instructions backward)
 input_cube_reset_instructions = []
+
+# This set is a set of tuples that, together, represent which cubes are on.
+# Each tuple in the is a pair of 3-tuples with the opposite corners of a "block," one has minimum (x,y,z) values and the other has the maximum (x,y,z) values.
 blocks_on = set()
 
+# This function calculates and returns the number of cubes in a given block
+# (Note that this function should work for any number of dimensions)
 def get_volume_from_block(block):
     ret_val = 1
     for i in range(len(block[0])):
@@ -11,11 +19,14 @@ def get_volume_from_block(block):
     return ret_val
 
 # Detects any clash between the two input blocks
+# "Clash" is defined as any cubes the two blocks have in common
 # Returns None if there is no clash.
 # Returns a block of the region with a clash if there is any clash
+# (Note that this function should work for any number of dimensions)
 def get_clash(block1, block2):
     min_pt = []
     max_pt = []
+    # iterate over axes:  x, y, z
     for i in range(len(block1[0])):
         # Look for two scenarios without any overlaps in this axis
         if block1[1][i] < block2[0][i]:
@@ -29,30 +40,40 @@ def get_clash(block1, block2):
 
     # Only if all three axes have a clash.
     return (tuple(min_pt), tuple(max_pt))
+# end of function get_clash
 
 # This breaks up block into pieces that completely cover the non-clash region of block
 # Only those pieces (covering non-clash region) are returned
+# (Note that this function should work for any number of dimensions)
 def break_up(block_whole, block_clash):
     block_remaining_after_breakup = [list(x) for x in block_whole]
     ret_val = []
+    # For each of the axes (for example, x and y and z)
     for i in range(len(block_whole[0])):
+        # If the clash isn't at the minimum of this axis, then break off a piece less than that minimum
         if block_remaining_after_breakup[0][i] != block_clash[0][i]:
             block_to_add = [list(x) for x in block_remaining_after_breakup]
             block_to_add[1][i] = block_clash[0][i] - 1
             block_to_add = (tuple(x) for x in block_to_add)
             ret_val.append(tuple(block_to_add))
 
+        # If the clash isn't at the maximum of this axis, then break off a piece greater than that maximum
         if block_remaining_after_breakup[1][i] != block_clash[1][i]:
             block_to_add = [list(x) for x in block_remaining_after_breakup]
             block_to_add[0][i] = block_clash[1][i] + 1
             block_to_add = [tuple(x) for x in block_to_add]
             ret_val.append(tuple(block_to_add))
 
+        # When finished with this axis (axis i), store results so any and all break-offs for the rest of the axes won't break this off again
         block_remaining_after_breakup[0][i] = block_clash[0][i]
         block_remaining_after_breakup[1][i] = block_clash[1][i]
-
+    # Return all pieces that were broken off to handle the non-clash regions
     return ret_val
+# end of function break_up
 
+# This class is used to load the input instructions into memory.
+# (This was developed in part A, under the incorrect assumption 
+# that this might perhaps be useful in part B)
 class CubeResetInstruction:
     # Load a line of input instructions into memory
     def __init__(self, str_input):
@@ -66,16 +87,18 @@ class CubeResetInstruction:
     # End of __init__ declaration
 
     # Execute this one instruction
+    # This function explicitly is for three dimensions labelled x,y,and z
     def execute_instruction(self):
         min_pt = []
         max_pt = []
+
+        # Construct newest_block for this instruction
         axes = ['x', 'y', 'z']
         for axis in axes:
             min_pt.append(self.ranges[axis][0])
             max_pt.append(self.ranges[axis][1])
         newest_block = (tuple(min_pt), tuple(max_pt))
 
-        # Additional loop needed here .... while True:
         flag_whileTrue = True
         while flag_whileTrue:
             flag_whileTrue = False
@@ -99,13 +122,10 @@ class CubeResetInstruction:
         if self.operation == 'on':
             blocks_on.add(newest_block)
 
+    # end of function execute_instruction
+# end of class CubeResetInstruction
             
-            
-        # start over with iteration to see if any other elements of the set clash
-        
-    
-
-# Reading input from the input file
+# Read input from the input file
 input_filename='input.txt'
 with open(input_filename) as f:
     del input_filename
@@ -121,13 +141,15 @@ del cri
 del in_string
 del f
 
+# Execute instructions
 for icri in input_cube_reset_instructions:
     icri.execute_instruction()
 
+# Count number of cubes (the solution to the problem)
 number_of_cubes = 0
-
 for block in blocks_on:
     number_of_cubes += get_volume_from_block(block)
 
+# Display result to user
 print('The answer to part B is: ', end='')
-print(number_of_cubes) # eventual answer to the problem
+print(number_of_cubes) # answer to Day 22, part B
