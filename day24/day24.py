b410b9d6480576a059a5c000661c22360b768408
Lewis Staples <lewis.staples@gmail.com>
2022-01-23 22:52:16 -0500

    Day 24 Starting to code the ALU

---
+++ day24/day24.py
@@ -0,0 +1,26 @@
+# adventOfCode 2021 day ??
+# https://adventofcode.com/2021/day/??
+
+class ALU:
+    # This reads the ALU program from an input file
+    def __init__(self):
+        input_filename='input_sample0.txt'
+        self.instructions = []
+        # Reading input from the input file
+        with open(input_filename) as f:
+            # Pull in each line from the input file
+            for in_string in f:
+                in_string = in_string.rstrip()
+                self.instructions.append(in_string)
+
+    # This executes the ALU program against an input number
+    def execute(self, input_num):
+        w = x = y = z = 0
+        for i_ins in range(len(self.instructions)):
+            print(self.instructions[i_ins])
+        print('x = ', end='')
+        print(x, end=', ')
+# Main program
+the_alu = ALU()
+the_alu.execute(2)
+
