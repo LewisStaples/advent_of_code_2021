a33660613d0bace165a09176d2367e27236db16f
Lewis Staples <lewis.staples@gmail.com>
2022-01-31 23:25:22 -0500

    Day 24 Breaking MONAD into 14 components, each of which starts with an inp instruction

--- day24/day24.py
+++ day24/day24.py
@@ -4,27 +4,47 @@
 class ALU:
     # This reads the ALU program from an input file
     def __init__(self):
-        input_filename='input_sample0.txt'
+        input_filename='input_sample1.txt'
         self.instructions = []
+        this_instruction_list = None
         # Reading input from the input file
         with open(input_filename) as f:
             # Pull in each line from the input file
             for in_string in f:
                 in_string = in_string.rstrip()
-                self.instructions.append(in_string)
+                if in_string[:3] == 'inp':
+                    if this_instruction_list is not None:
+                        self.instructions.append(this_instruction_list)
+                    this_instruction_list = []
+                this_instruction_list.append(in_string)
+        self.instructions.append(this_instruction_list)
 
-    # This executes the ALU program against one value in a list of input numbers
-    def execute(self, input_numbers, number_index):
-        # Variables defined in the problem specification:
-        w = x = y = z = 0
-        #
-        input_number = 0
-        for i_ins in range(len(self.instructions)):
-            print(self.instructions[i_ins])
-        print('x = ', end='')
-        print(x, end=', ')
+    def inp(self, param):
+        print('inp ' + param)
+
+    def mul(self, param):
+        print('mul', end=' ')
+        for par in param.split(' '):
+            print(par, end = ' ')
+        print()
+
+    def eql(self, param):
+        print('eql', end=' ')
+        for par in param.split(' '):
+            print(par, end = ' ')
+        print()
+
+    def execute(self):
+        for instruction_list in self.instructions:
+            for instruction in instruction_list:
+                instruction_line = instruction.split(' ',1)
+                # dummy = 123
+                call_fxn = getattr(self, instruction_line[0]) #(instruction_line[1])
+                call_fxn(instruction_line[1])
+            #     print(instruction)
+            # print('------------')
 
 # Main program
 the_alu = ALU()
-the_alu.execute(2)
+the_alu.execute()
 
