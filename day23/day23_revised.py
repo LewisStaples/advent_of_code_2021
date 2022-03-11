9d6d0666e85435d035623e3dbfbfb860b68644dc
Lewis Staples <lewis.staples@gmail.com>
2022-03-10 23:41:37 -0500

    Day 23 Coding sending from one sideroom to another sideroom

--- day23/day23_revised.py
+++ day23/day23_revised.py
@@ -29,7 +29,7 @@ least_total_energy = float('inf')
 sideroom_indices = []
 
 
-input_filename='input_sample0.txt'
+input_filename='input_scenario2.txt'
 # Reading input from the input file
 print()
 print('Reading input from ', end='')
@@ -43,27 +43,33 @@ with open(input_filename) as f:
     for row_num, in_string in enumerate(f):
         if row_num == 0:
             # Require line to be all pound characters
-            line_length = len(in_string) - 1
+            line_length = len(in_string.rstrip())
             if in_string.rstrip() != '#'*line_length:
                 sys.exit('Bad input!')
-        elif row_num == 1:
-            # Require line to have one pound at start and finish.  All interior characters must be periods
-            if in_string.rstrip() != '#'+'.'*(line_length-2)+'#':
+        elif row_num == 1:            
+            # Require first and last character to be #
+            # if in_string.rstrip() != '#'+'.'*(line_length-2)+'#':
+            if in_string[0] != '#' or in_string.rstrip()[-1] != '#':
                 sys.exit('Bad input!')
             
-            # All hallway positions are initially unoccupied
+            # Initialize list with all hallway positions unoccupied
             burrow_state_list[0][0].extend([None]*(line_length-2))
-            dummy = 123
+
+
+            for i,ch in enumerate(in_string):
+                if ch in AMPHIPOD_LIST:
+                    burrow_state_list[0][0][i-1] = ch
+            pass
 
         elif row_num == 2:
             # find the letters and their side room position
             # replace None with the named 2-tuple (described above)
             for i,ch in enumerate(in_string):
-                if ch.isalpha():
+                if ch.isalpha() or ch == '.':
                     # This hallway position is a side room.  Add first amphipod
                     dest_char = AMPHIPOD_LIST[len(side_room_position)]
                     side_room_position[i] = dest_char
-                    burrow_state_list[0][0][i-1] = (dest_char,[ch])
+                    burrow_state_list[0][0][i-1] = (dest_char,[ch]) if ch!='.' else (dest_char, [None])
                     sideroom_indices.append(i-1)
                     
             dummy = 123
@@ -82,24 +88,33 @@ dummy = 123
 def transfer_amphipod(burrow_state_list, i_origin, i_dest):
     # If there are no obstacles on the journey, create a new state in state_list but with the amphipod transferred and increase the energy and add this to the end of the list.
     energy_total = burrow_state_list[0][1]
+
+    # Identify the amphipod being transferred
+    amph_to_transfer = burrow_state_list[0][0][i_origin[0]] if i_origin[1] == None else burrow_state_list[0][0][i_origin[0]][1][i_origin[1]]
+
     # Logic if origin is a sideroom
     if i_origin[1] is not None:
         sideroom_position = i_origin[1]
+        # While still in the origin sideroom, take one step toward the hallway (up)
         while sideroom_position > -1:
+            # Increment two variables directly impacted by taking a step
             sideroom_position -= 1
             energy_total += 1
-            if sideroom_position > -1 and isinstance(burrow_state_list[0][0][sideroom_position], str):
+            # If still in the sideroom, check if there is an amphipod blocking the route
+            if sideroom_position > -1 and \
+            isinstance(burrow_state_list[0][0][i_origin[0]][1][sideroom_position], str):
+                # Blocking amphipod detected, therefore cancel the transfer
                 return False
 
         # Increment energy for step from sideroom to the hallway
         energy_total += 1
     
-
-    
-    tran_dir = 1 if i_origin[0] < i_dest[0] else -1
+    # Logic used in all transfers
+    # Determine transfer direction in hallway (left or right)
+    tran_dir = 1 if i_origin[0] < i_dest[0] else -1 
     hallway_posn = i_origin[0] + tran_dir
     energy_total += 1
-    while hallway_posn != i_dest[0]:
+    while hallway_posn != i_dest[0] and hallway_posn > -1:
         if isinstance(burrow_state_list[0][0][hallway_posn], str):
             return False
         hallway_posn += tran_dir
@@ -107,21 +122,45 @@ def transfer_amphipod(burrow_state_list, i_origin, i_dest):
 
 
     # Logic if destination is a sideroom
-    # (To be added later)
+    if i_dest[1] is not None:
+        # Verify that this destination sideroom is where the amphipod should be going
+        if amph_to_transfer != burrow_state_list[0][0][i_dest[0]][1]:
+            return False
+
+        # Repeating commands used for origin
+        sideroom_position = i_dest[1]
+
+        # Take one (imaginary) step toward the hallway (up)
+        # (The step is imaginary, because the direction is actually reversed)
+        while sideroom_position > -1:
+            # Increment two variables directly impacted by taking a step
+            sideroom_position -= 1
+            energy_total += 1
+            # If still in the sideroom, check if there is an amphipod blocking the route
+            if sideroom_position > -1 and \
+            isinstance(burrow_state_list[0][0][i_dest[0]][1][sideroom_position], str):
+                # Blocking amphipod detected, therefore cancel the transfer
+                return False
+
+        # Increment energy for step from sideroom to the hallway
+        energy_total += 1
 
 
     # The transfer will happen (because otherwise False would have been returned earlier)
     new_hallway = copy.deepcopy(burrow_state_list[0][0])
-    # NEED TO ADD ... replace origin amph. with None, add dest amph. (replace the None there) in the copy.  Then put energy in with a tuple
-    amph_to_transfer = new_hallway[i_origin[0]] if i_origin[1] == None else new_hallway[i_origin[0]][1][i_origin[1]]
+    
+    # Remove amphipod from origin
     if i_origin[1] == None:
         new_hallway[i_origin[0]] = None  
     else:
         new_hallway[i_origin[0]][1][i_origin[1]] = None
+    
+    # Add amphipod at destination
     if i_dest[1] == None:
         new_hallway[i_dest[0]] = amph_to_transfer
     else:
         new_hallway[i_dest[0]][1][i_dest[1]] = amph_to_transfer
+
     burrow_state_list.append((new_hallway, energy_total))
     return True
 
@@ -147,14 +186,15 @@ def get_siderooms(burrow_state_list, in_str):
             else:
                 lowest_occupied = min(lowest_occupied, i_amphipod)
 
-        # # NOT YET TESTED, SO COMMENTING IT OUT FOR NOW
-        # if in_str == 'dest':
-        #     # Since all amphipods that are here belong here, any None can be used
-        #     for i_amphipod in range(len(sideroom[1])-1,-1,-1):
-        #         if sideroom[1][i_amphipod] is None:
-        #             ret_val.append((sideroom_index, i_amphipod))
-        #             break
-        #     ret_val.append((sideroom_index, i_amphipod))
+        # NOT YET TESTED, SO COMMENTING IT OUT FOR NOW
+        if in_str == 'dest':
+            # Since all amphipods that are here belong here, any None can be used
+            # Traverse all slots in the sideroom, from bottom to top
+            for i_amphipod in range(len(sideroom[1])-1,-1,-1):
+                if sideroom[1][i_amphipod] is None:
+                    ret_val.append((sideroom_index, i_amphipod))
+                    break
+            ret_val.append((sideroom_index, i_amphipod))
 
     return ret_val
 
@@ -163,6 +203,8 @@ def get_hallways(burrow_state_list, in_str):
     for i_elem,elem in enumerate(burrow_state_list[0][0]):
         if elem is None and in_str == 'dest':
             ret_val.append((i_elem, None))
+
+        # # NOT YET TESTED, SO COMMENTING IT OUT FOR NOW
         # elif isinstance(elem, str) and in_str == 'origin':
         #     ret_val.append((i_elem, None))
     return ret_val
@@ -180,72 +222,19 @@ def next_move(burrow_state_list):
     # 3. Move from sideroom to hallway (if conditions allow it, and if 1 isn't possible)
     #   (3) is skipped if (1) is possible, because (1) must be faster
 
+    # Send an amphipod from origin sideroom directly to destination sideroom
+    for tran_origin in get_siderooms(burrow_state_list, 'origin'):
+        for tran_dest in get_siderooms(burrow_state_list, 'dest'):
+            if transfer_amphipod(burrow_state_list, tran_origin, tran_dest):
+                return
+
+    # Send an amphipod from the hallway directly to destination sideroom
+    # !!! NEEDED !!!
+
     # Send an amphipod from a sideroom to a hallway location
     for tran_origin in get_siderooms(burrow_state_list, 'origin'):
         for tran_dest in get_hallways(burrow_state_list, 'dest'):
             transfer_amphipod(burrow_state_list, tran_origin, tran_dest)
-
-
-
-    # # Check all siderooms if eligible to receive an amphipod.
-    # for sideroom_dest in sideroom_indices:
-    #     eligible = True
-    #     for i_dest, amp_dest in enumerate(burrow_state_list[0][0][sideroom_dest][1]):
-    #         # if any that aren't the destination amphipod, then it's ineligible
-    #         # otherwise it's eligible (if empty or with 1+ destination amphipods)
-    #         if amp_dest != burrow_state_list[0][0][sideroom_dest][0]:
-    #             eligible = False
-    #     if eligible:
-    #         for sideroom_origin in sideroom_indices:
-    #             if sideroom_dest==sideroom_origin:
-    #                 continue
-    #             for i_origin,amp_origin in enumerate(burrow_state_list[0][0][sideroom_origin][1]):
-    #                 if amp_origin is None:
-    #                     continue
-    #                 if amp_origin == amp_dest:
-    #                     # if transfer_amphipod(burrow_state_list[0][0][sideroom_origin][1], i_origin, burrow_state_list[0][0], i_dest):
-    #                     if True:
-    #                         break
-    #                 break
-
-    # Check all hallway locations for amphipods that could be sent to destination sideroom.
-
-
-    # # Send an amphipod from a sideroom to a hallway location
-    # for sideroom_origin in sideroom_indices:
-    #     send_to_hallway = True
-    #     # Abbreviate content about the sideroom origin. 
-    #     siderm_orgn = burrow_state_list[0][0][sideroom_origin]
-    #     # Try another sideroom unless there is at least one amphipod (not None) that isn't already at its destination.
-    #     i_origin = None
-    #     for i_origin, amp_origin in enumerate(siderm_orgn[1]):
-    #         # If sideroom slot is at its intended final destination,
-    #         if amp_origin == siderm_orgn[0]:
-    #             # Look to see if any subsequent sideroom slots aren't at final destination
-    #             send_to_hallway = False
-    #             for amp_origin_subsequent in siderm_orgn[1][i_origin:]:
-    #                 if amp_origin_subsequent != amp_origin:
-    #                     send_to_hallway = True
-    #                     break
-    #             break
-    #         # If the origin sideroom slot is empty, try another slot.
-    #         if amp_origin is None:
-    #             continue
-    #         # Stop looking at other slots in the origin sideroom
-    #         break
-
-    #     # Send to a hallway location
-    #     if send_to_hallway:
-    #         for i_dest, hallway_dest in enumerate(burrow_state_list[0][0]):
-    #             if hallway_dest is None:
-    #                 transfer_amphipod(burrow_state_list, (sideroom_origin, i_origin), (i_dest, None))
-    #                     # break
-    #                 # continue
-
-    #         #     # Do not try any amphipods in sideroom_origin after the first one
-    #         #     break
-
-
 
     # When this point gets reached, remove the 0th element from the list, because all next moves have been exhausted
     burrow_state_list.pop(0)
@@ -256,7 +245,7 @@ def next_move(burrow_state_list):
 
 # Code for testing / development only
 next_move(burrow_state_list)
-next_move(burrow_state_list)
+# next_move(burrow_state_list)
 
 for i in range(len(burrow_state_list)):
     # display = False
