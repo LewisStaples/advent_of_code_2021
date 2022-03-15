e4c9f8a2685b2fc41a16a24f2d3522919802c75a
Lewis Staples <lewis.staples@gmail.com>
2022-03-14 23:40:36 -0400

    Day 23 Continuing conversion to classes

--- day23/day23_classes.py
+++ day23/day23_classes.py
@@ -5,14 +5,73 @@
 
 import sys
 
+from day23_revised import AMPHIPOD_LIST
+
+class SideRoom:
+    pass
+
+class HallwayRoom:
+    pass
+
+class BurrowState:
+    # This code is only run once ... while reading from input.
+    def __init__(self, hallway_init):
+        self.hallway = []
+        if isinstance(hallway_init, str):
+            self.energy_total = 0
+            # Remove # and newline characters
+            hallway_init = hallway_init.rstrip()[1:-1]
+            for ch in hallway_init:
+                if ch == '.':
+                    self.hallway.append(None)
+                elif ch in Burrow.AMPHIPOD_LIST:
+                    self.hallway.append(ch)
+                else:
+                    sys.exit('Bad input character: ' + ch)
+            dummy = 123
+
+        # This code is run multiple times ... whenever duplicating a BurrowState.
+        elif isinstance(hallway_init, BurrowState):
+            self.energy_total = hallway_init.energy_total
+        
+    def sideroom_init(self, sideroom_str):
+        # Discover where the side rooms are
+        #
+        # Note that my input samples include a '.' in siderooms,
+        # because I wanted to test an alternate scenario where
+        # amphipods were already in the hallway.
+
+        al_index = 0 # temp. variable:  index in Burrow.AMPHIPOD_LIST
+        for i,ch in enumerate(sideroom_str):
+            if ch.isalpha() or ch == '.':
+                # This hallway position is a side room.  Add first amphipod
+                al_index += 1
+                self.hallway[i-1] = ([ch])
+                Burrow.SIDEROOM_INDICES.append(i-1)
+
+    def sideroom_append(self, sideroom_str):
+                    # for i,ch in enumerate(in_string):
+                    #     if ch.isalpha():
+                    #         # burrow_state_list[0][0][i-1][1].append(ch)
+                    #         pass
+        pass
+
 # Burrow will
 class Burrow:
-    def __init__(self, input_filename):
+    # List of all amphipods (in alphabetical order, which is the desired order when the process will be completed)
+    AMPHIPOD_LIST = ['A', 'B', 'C', 'D']
+    # List of indices of siderooms
+    SIDEROOM_INDICES = []
 
-        # List of all amphipods (in alphabetical order, which is the desired order when the process will be completed)
-        self.AMPHIPOD_LIST = ['A', 'B', 'C', 'D']
-        # This dictionary has index = 'type of amphipod' and value is a list with energy and the hallway location that goes into its destination room.  These are immutable characteristics (it will be populated from the input file and it will not change afterward)
-        self.AMPHIPOD_CHARACTERISTICS = {'A':[1], 'B':[10], 'C':[100], 'D':[1000]}
+    # Create pair of dictionaries to lookup destination amphipod by sideroom index and vice versa.
+    DEST_AMPH__SIDEROOM_INDEX = dict()
+    SIDEROOM_INDEX__DEST_AMPH = dict()
+
+    # This dictionary has index = 'type of amphipod' and value is the energy.
+    AMPHIPOD_ENERGY = {'A':[1], 'B':[10], 'C':[100], 'D':[1000]}
+
+    def __init__(self, input_filename):
+        self.all_burrowStates = []
 
         # Open the input file
         print('Using input file: ', end='')
@@ -30,39 +89,24 @@ class Burrow:
                     self.HALLWAY_LENGTH = line_length -2
                 elif row_num == 1:            
                     # Require first and last character to be #
-                    # if in_string.rstrip() != '#'+'.'*(line_length-2)+'#':
                     if in_string[0] != '#' or in_string.rstrip()[-1] != '#':
                         sys.exit('Bad input!')
                     
-                    # # Initialize list with all hallway positions unoccupied
-                    # burrow_state_list[0][0].extend([None]*(line_length-2))
-
-
-                    for i,ch in enumerate(in_string):
-                        if ch in self.AMPHIPOD_LIST:
-                            # burrow_state_list[0][0][i-1] = ch
-                            pass
-
+                    # Create the first BurrowState
+                    self.all_burrowStates.append(BurrowState(in_string))
 
                 elif row_num == 2:
-                    # find the letters and their side room position
-                    # replace None with the named 2-tuple (described above)
-                    for i,ch in enumerate(in_string):
-                        if ch.isalpha() or ch == '.':
-                            # This hallway position is a side room.  Add first amphipod
-                            dest_char = self.AMPHIPOD_LIST[len(side_room_position)]
-                            side_room_position[i] = dest_char
-                            # burrow_state_list[0][0][i-1] = (dest_char,[ch]) if ch!='.' else (dest_char, [None])
-                            # sideroom_indices.append(i-1)
-                            
+                    self.all_burrowStates[0].sideroom_init(in_string)
+
                 else:
-                    for i,ch in enumerate(in_string):
-                        if ch.isalpha():
-                            # burrow_state_list[0][0][i-1][1].append(ch)
-                            pass
+                    self.all_burrowStates[0].sideroom_append(in_string)
+
+        for i in range(len(Burrow.AMPHIPOD_LIST)):
+            Burrow.DEST_AMPH__SIDEROOM_INDEX[AMPHIPOD_LIST[i]] = Burrow.SIDEROOM_INDICES[i]
+            Burrow.SIDEROOM_INDEX__DEST_AMPH[Burrow.SIDEROOM_INDICES[i]] = AMPHIPOD_LIST[i]
 
 class HallwayRoom:
     pass
 
-theBurrow = Burrow('input.txt')
+theBurrow = Burrow('input_scenario2.txt')
 
