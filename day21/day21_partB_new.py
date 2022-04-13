b83a4bdc8f1f43af8ea98db4239abc50c8c42ebe
Lewis Staples <lewis.staples@gmail.com>
2022-04-12 22:48:32 -0400

    Day 21 starting multiple player rounds

--- day21/day21_partB_new.py
+++ day21/day21_partB_new.py
@@ -14,17 +14,28 @@ for player1_score in range(20, 18, -1):
 #     for player2_score in range(20, -1, -1):
         for player1_space in range(1, 2):
             for player2_space in range(1, 2):
-                this_roll_input = Roll_Input(player1_score, player2_score, player1_space, player2_space)
         # for player1_space in range(1, 11):
         #     for player2_space in range(1, 11):
-                for roll in range(1, 4):
-                    player1_score_final = (player1_score + roll) % 10
-                    player1_wins_final = 0
-                    player2_wins_final = 0
-                    print(roll, end=': ')
-                    if player1_score+roll > 20:
-                        player1_wins_final += 1
-                    game_outcome_dict[this_roll_input] = Roll_Output(player1_wins_final, player2_wins_final) # player one has won
+                this_roll_input = Roll_Input(player1_score, player2_score, player1_space, player2_space)
+                player1_wins = 0
+                player2_wins = 0
+                for roll1 in range(1, 4):
+                    player1_score_round = (player1_score + roll1)
+                    player1_space_round = (player1_space + roll1) % 10
+
+                    # print(roll1, end=': ')
+                    if player1_score_round > 20:
+                        player1_wins += 1
+                    else:
+                        for roll2 in range(1, 4):
+                            player2_score_round = (player2_score + roll2)
+                            player2_space_round = (player2_space + roll2) % 10
+                            if player2_score_round > 20:
+                                player2_wins += 1
+                            else:
+                                player2_wins += game_outcome_dict[Roll_Input(player1_score_round, player2_score_round,player1_space_round, player2_space_round)].FinalPlayerOneScore
+                                player1_wins += game_outcome_dict[Roll_Input(player1_score_round, player2_score_round,player1_space_round, player2_space_round)].FinalPlayerTwoScore
+                    game_outcome_dict[this_roll_input] = Roll_Output(player1_wins, player2_wins) # player one has won
 
 dummy = 123
 
---
+++ day21/readme_scratch.txt
@@ -0,0 +1,19 @@
+
+
+The given number of game counts is too large to play out every game to the end!
+
+Instead start by considering what happens if the score at the start of a round is 20 and that player is at any of the spaces 1-10.  Then consider a starting score of 19.  Therefore, the table will have 210 entries.
+
+NO ... the table will have 210^2 = 44,100 entries to support starting score and starting space for both players
+
+Given starting score 0-20, and given starting space 1-10, how many wins go for to starting player and the other player?
+
+Data Structure is dict with index named tuple
+	first_player_space
+	first_player_score
+	second_player_space
+	second_player_score
+
+
+
+
