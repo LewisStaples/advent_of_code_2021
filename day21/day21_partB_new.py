1f175d12f114064e8dca4520acc537e8591eb6ae
Lewis Staples <lewis.staples@gmail.com>
2022-04-14 22:17:22 -0400

    Day 21 Complete

--- day21/day21_partB_new.py
+++ day21/day21_partB_new.py
@@ -12,20 +12,11 @@ def handle_space_overflow(space_in):
         space_out -= 10
     return space_out
 
-# for i in range(1,13):
-#     print(i, end='   ')
-#     print(handle_space_overflow(i))
-# print()
-
 # player1 is defined to be the player who has the next turn
 # Any calls to a prior game_outcome_dict will reverse the players' identities
 game_outcome_dict = dict()
-# for player1_score in range(20, 19, -1):
-#     for player2_score in range(20, 19, -1):
 for player1_score in range(20, -1, -1):
     for player2_score in range(20, -1, -1):
-        # for player1_space in range(1, 2):
-        #     for player2_space in range(1, 2):
         for player1_space in range(1, 11):
             for player2_space in range(1, 11):
                 this_roll_input = Roll_Input(player1_score, player2_score, player1_space, player2_space)
@@ -48,26 +39,20 @@ for player1_score in range(20, -1, -1):
                                             if player2_score_round > 20:
                                                 player2_wins += 1
                                             else:
-                                                try:
-                                                    player2_wins += game_outcome_dict[Roll_Input(player1_score_round, player2_score_round, player1_space_round, player2_space_round)].PlayerTwoWinCount
-                                                except KeyError:
-                                                    print('Error!')
-                                                    print(player1_score_round)
-                                                    print(player2_score_round)
-                                                    print(player1_space_round)
-                                                    print(player2_space_round)
-                                                    sys.exit('KeyError!')
-
+                                                player2_wins += game_outcome_dict[Roll_Input(player1_score_round, player2_score_round, player1_space_round, player2_space_round)].PlayerTwoWinCount
                                                 player1_wins += game_outcome_dict[Roll_Input(player1_score_round, player2_score_round,player1_space_round, player2_space_round)].PlayerOneWinCount
                             game_outcome_dict[this_roll_input] = Roll_Output(player1_wins, player2_wins) # player one has won
 
-dummy = 123
+playerOneCount = game_outcome_dict[Roll_Input(InitialPlayerOneScore=0, InitialPlayerTwoScore=0, PlayerOneSpace=4, PlayerTwoSpace=8)].PlayerOneWinCount
+playerTwoCount = game_outcome_dict[Roll_Input(InitialPlayerOneScore=0, InitialPlayerTwoScore=0, PlayerOneSpace=4, PlayerTwoSpace=8)].PlayerTwoWinCount
 
-print('Part B sample results are: ', end='')
-try:
-    print(game_outcome_dict[Roll_Input(InitialPlayerOneScore=0, \
-    InitialPlayerTwoScore=0, \
-    PlayerOneSpace=4, \
-    PlayerTwoSpace=8)])
-except Exception:
-    sys.exit('\nError when attempting to print!')
+print('Part B sample results are: ')
+print('Player One Win Count: ', end='')
+print(playerOneCount)
+print('Player Two Win Count: ', end='')
+print(playerTwoCount)
+
+print('The value to submit is (the greater of the above): ', end='')
+print(max(playerOneCount, playerTwoCount))
+
+
