647e45bd26daeab17dfc34b3ee5e8ecc093b3cf2
Lewis Staples <lewis.staples@gmail.com>
2022-04-16 23:45:11 -0400

    Day 19  Continuing to implement process for transforming scanners

--- day19/day19.py
+++ day19/day19.py
@@ -19,7 +19,7 @@ class Scanner:
                 length_squared = 0
                 for comp_index in range(len(self.point_list[p1_index])):
                     length_squared += (self.point_list[p1_index][comp_index] - self.point_list[p2_index][comp_index])**2
-                self.edges[(p1_index, p2_index)] = length_squared
+                self.edges[length_squared] = (p1_index, p2_index)
                 self.lengths_squared.add(length_squared)
         dummy = 123
 
@@ -29,14 +29,27 @@ scannerListTransformed = []
 # For 12 common beacons there should be 1+2+...+11 = 66 This may be proven using recursive logic where adding a new point to a graph of n points creates n new edges. So start with n=1 point with no initial edges, then adding a second point adds a single edge (1 in total), then adding a third point yields two new edges (3 total), and so on.
 REQUIRED_NUMBER_OF_EQUAL_LENGTH_EDGES = 66
 
-def transform_scanner():
-    for i_tr, this_scanner_transformed in enumerate(scannerListTransformed):
-        for i_untr,this_scanner_untransformed in enumerate(scannerListUntransformed):
+def get_scanner_pair():
+    for this_scanner_transformed in scannerListTransformed:
+        for this_scanner_untransformed in scannerListUntransformed:
             if len(this_scanner_transformed.lengths_squared.intersection(this_scanner_untransformed.lengths_squared)) > 11:
-                print('match!')
-                scannerListTransformed.append(this_scanner_untransformed)
-                scannerListUntransformed.remove(this_scanner_untransformed)
-                return
+                return (this_scanner_transformed, this_scanner_untransformed)
+
+def transform(this_scanner_transformed, this_scanner_untransformed):
+    # Find two edges that are in both scanners
+    lengths_squared_in_common = this_scanner_transformed.lengths_squared.intersection(this_scanner_untransformed.lengths_squared)
+    edge0 = lengths_squared_in_common.pop()
+    edge1 = lengths_squared_in_common.pop()
+
+    # Find a point that is in edge0 and edge1.  This point is now known for both scanners, as well as two other points (the points that are in one edge and not the other one)
+
+    # Use one of these points to define the (x,y) displacement between the scanners.  Use this (x,y) displacement, along with the 24 available rotations to identify the rotation that works for the other two untransformed points.
+
+    # Apply the known (x,y) displacement and rotation to all untransformed points
+
+    # Relabel the untransformed scanner list as now having been transformed
+    scannerListTransformed.append(this_scanner_untransformed)
+    scannerListUntransformed.remove(this_scanner_untransformed)
 
 # reading input from the input file
 input_filename='input.txt'
@@ -54,11 +67,14 @@ with open(input_filename) as f:
     # get last line
     scannerListUntransformed[-1].determine_edges()
 
-while len(scannerListUntransformed) > 0:
-    # Remove one Scanner from cannerListUntransformed and add it as-is to cannerListTransformed
-    this_scanner_untransformed = scannerListUntransformed.pop()
-    scannerListTransformed.append(this_scanner_untransformed)
-    transform_scanner()
-
+# Remove one Scanner from cannerListUntransformed and add it as-is to cannerListTransformed
+this_scanner_untransformed = scannerListUntransformed.pop()
+scannerListTransformed.append(this_scanner_untransformed)
 dummy = 123
 
+while len(scannerListUntransformed) > 0:
+    scanner_pair = get_scanner_pair()
+    transform(*scanner_pair)
+
+print('The answer is:')
+print('(To be determined)')
